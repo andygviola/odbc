f909ee829bf8c11f56acd4299aa1d85037dad2fb
Andrea G Viola <andrea.viola@icadsistemi.com>
2025-09-16 16:48:29 +0200

    [ADD] ARM64 and AMD64 separated

--- api/api_unix.go
+++ api/api_unix.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin linux freebsd
-// +build cgo
+//go:build linux && cgo
+// +build linux,cgo
 
 package api
 
